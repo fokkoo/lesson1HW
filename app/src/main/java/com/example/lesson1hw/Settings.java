af7c2613c93191d9a02bdce342c728c1db289387
KazanskiiPN <fokkoo@yandex.ru>
2021-07-19 16:10:39 +0300

    The button push return to main activity added

--- app/src/main/java/com/example/lesson1hw/Settings.java
+++ app/src/main/java/com/example/lesson1hw/Settings.java
@@ -3,6 +3,7 @@ package com.example.lesson1hw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        initGoToMainActivity();
+    }
+
+    private void initGoToMainActivity() {
+        findViewById(R.id.buttonGoToCalcunator).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
