42907a234c4ddffe918eccaf77a490365f361997
KazanskiiPN <fokkoo@yandex.ru>
2021-07-22 17:28:46 +0300

    The Radiobuttons work in settings menu now

--- app/src/main/java/com/example/lesson1hw/Settings.java
+++ app/src/main/java/com/example/lesson1hw/Settings.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class Settings extends AppCompatActivity {
-    //settings activ
+    
     public static final String NAME_SHARED_PREFERENCE = "LOGIN";
     public static final String appTheme = "APP_THEME";
 
